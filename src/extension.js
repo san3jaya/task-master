f0a837575693852e5c2136c41edc9ec53bb073c6
Sanjaya De Silva <sanjaya.de@visma.com>
2024-07-04 15:09:50 +0200

    ran the formatter

--- src/extension.js
+++ src/extension.js
@@ -1,51 +1,65 @@
-const vscode = require('vscode');
-const TaskProvider = require('./taskProvider');
+const vscode = require("vscode");
+const TaskProvider = require("./taskProvider");
 const TASK_CATEGORIES = ["Work", "Personal", "Urgent", "Misc"];
 
 function activate(context) {
-    const taskProvider = new TaskProvider(context);
-    vscode.window.registerTreeDataProvider('tasksView', taskProvider);
+  const taskProvider = new TaskProvider(context);
+  vscode.window.registerTreeDataProvider("tasksView", taskProvider);
 
-    // Register the command for adding a new task
-    let addDisposable = vscode.commands.registerCommand('taskMaster.addTask', function () {
-        vscode.window.showInputBox({ placeHolder: 'Add a new task' }).then(value => {
-            
-                vscode.window.showQuickPick(TASK_CATEGORIES, { placeHolder: 'Select a category' }).then(taskCategory => {
-                    if (!taskCategory) {
-                      vscode.window.showWarningMessage('You must select a category.');
-                      return;
-                    }
-                    // Add the task with the collected information
-                    const task = { label: value, category: taskCategory };
-                    taskProvider.addTask(task); // Assuming addTask() is already defined to handle such objects
-                });
+  // Register the command for adding a new task
+  let addDisposable = vscode.commands.registerCommand(
+    "taskMaster.addTask",
+    function () {
+      vscode.window
+        .showInputBox({ placeHolder: "Add a new task" })
+        .then((value) => {
+          vscode.window
+            .showQuickPick(TASK_CATEGORIES, {
+              placeHolder: "Select a category",
+            })
+            .then((taskCategory) => {
+              if (!taskCategory) {
+                vscode.window.showWarningMessage("You must select a category.");
+                return;
+              }
+              // Add the task with the collected information
+              const task = { label: value, category: taskCategory };
+              taskProvider.addTask(task); // Assuming addTask() is already defined to handle such objects
+            });
         });
-    });
+    }
+  );
 
-    // Register the command for updating an existing task
-    let updateDisposable = vscode.commands.registerCommand('taskMaster.updateTask', function (task) {
-        if (task) {
-            taskProvider.updateTask(task);
-        } else {
-            vscode.window.showWarningMessage('You must select a task to update.');
-        }
-    });
+  // Register the command for updating an existing task
+  let updateDisposable = vscode.commands.registerCommand(
+    "taskMaster.updateTask",
+    function (task) {
+      if (task) {
+        taskProvider.updateTask(task);
+      } else {
+        vscode.window.showWarningMessage("You must select a task to update.");
+      }
+    }
+  );
 
-    // Register the command for deleting an existing task
-    let deleteDisposable = vscode.commands.registerCommand('taskMaster.deleteTask', function (task) {
-        if (task) {
-            taskProvider.deleteTask(task);
-        } else {
-            vscode.window.showWarningMessage('You must select a task to delete.');
-        }
-    });
+  // Register the command for deleting an existing task
+  let deleteDisposable = vscode.commands.registerCommand(
+    "taskMaster.deleteTask",
+    function (task) {
+      if (task) {
+        taskProvider.deleteTask(task);
+      } else {
+        vscode.window.showWarningMessage("You must select a task to delete.");
+      }
+    }
+  );
 
-    context.subscriptions.push(addDisposable, updateDisposable, deleteDisposable);
+  context.subscriptions.push(addDisposable, updateDisposable, deleteDisposable);
 }
 
 function deactivate() {}
 
 module.exports = {
-    activate,
-    deactivate
+  activate,
+  deactivate,
 };
--- src/taskProvider.js
+++ src/taskProvider.js
@@ -1,66 +1,80 @@
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 class TaskProvider {
-    constructor(context) {
-        this.context = context;
-        this._onDidChangeTreeData = new vscode.EventEmitter();
-        this.onDidChangeTreeData = this._onDidChangeTreeData.event;
-        this.loadTasks();
-    }
+  constructor(context) {
+    this.context = context;
+    this._onDidChangeTreeData = new vscode.EventEmitter();
+    this.onDidChangeTreeData = this._onDidChangeTreeData.event;
+    this.loadTasks();
+  }
 
-    loadTasks() {
-        const storageScope = vscode.workspace.getConfiguration('taskMaster').get('storageScope');
-        const storedTasks = storageScope === 'workspace' ?
-            this.context.workspaceState.get('tasks', []) :
-            this.context.globalState.get('tasks', []);
-        this.tasks = storedTasks;
-    }
+  loadTasks() {
+    const storageScope = vscode.workspace
+      .getConfiguration("taskMaster")
+      .get("storageScope");
+    const storedTasks =
+      storageScope === "workspace"
+        ? this.context.workspaceState.get("tasks", [])
+        : this.context.globalState.get("tasks", []);
+    this.tasks = storedTasks;
+  }
 
-    saveTasks() {
-        const storageScope = vscode.workspace.getConfiguration('taskMaster').get('storageScope');
-        if (storageScope === 'workspace') {
-            this.context.workspaceState.update('tasks', this.tasks);
-        } else {
-            this.context.globalState.update('tasks', this.tasks);
+  saveTasks() {
+    const storageScope = vscode.workspace
+      .getConfiguration("taskMaster")
+      .get("storageScope");
+    if (storageScope === "workspace") {
+      this.context.workspaceState.update("tasks", this.tasks);
+    } else {
+      this.context.globalState.update("tasks", this.tasks);
+    }
+  }
+
+  getChildren() {
+    return this.tasks;
+  }
+
+  getTreeItem(task) {
+    let treeItem = new vscode.TreeItem(
+      `${task.label} -- ${task.category}]`,
+      vscode.TreeItemCollapsibleState.None
+    );
+    treeItem.contextValue = "task";
+    return treeItem;
+  }
+
+  addTask(taskItem) {
+    this.tasks.push(taskItem);
+    this.saveTasks();
+    this._onDidChangeTreeData.fire();
+  }
+
+  updateTask(task) {
+    vscode.window
+      .showInputBox({ placeHolder: "Update task", value: task.label })
+      .then((value) => {
+        if (value && this.tasks.indexOf(task) !== -1) {
+          task.label = value;
+          this.saveTasks();
+          this._onDidChangeTreeData.fire();
         }
-    }
+      });
+  }
 
-    getChildren() {
-        return this.tasks;
-    }
-
-    getTreeItem(task) {
-        let treeItem = new vscode.TreeItem(`${task.label} -- ${task.category}]`, vscode.TreeItemCollapsibleState.None);
-        treeItem.contextValue = 'task';
-        return treeItem;
-    }
-
-    addTask(taskItem) {
-        this.tasks.push(taskItem);
-        this.saveTasks();
-        this._onDidChangeTreeData.fire();
-    }
-    
-    updateTask(task) {
-        vscode.window.showInputBox({ placeHolder: 'Update task', value: task.label }).then(value => {
-            if (value && this.tasks.indexOf(task) !== -1) {
-                task.label = value;
-                this.saveTasks();
-                this._onDidChangeTreeData.fire();
-            }
-        });
-    }
-
-    deleteTask(task) {
-        vscode.window.showInformationMessage(`Are you sure you want to delete "${task.label}"?`, ...['Yes', 'No'])
-            .then(answer => {
-                if (answer === 'Yes' && this.tasks.indexOf(task) !== -1) {
-                    this.tasks = this.tasks.filter(t => t !== task);
-                    this.saveTasks();
-                    this._onDidChangeTreeData.fire();
-                }
-            });
-    }
+  deleteTask(task) {
+    vscode.window
+      .showInformationMessage(
+        `Are you sure you want to delete "${task.label}"?`,
+        ...["Yes", "No"]
+      )
+      .then((answer) => {
+        if (answer === "Yes" && this.tasks.indexOf(task) !== -1) {
+          this.tasks = this.tasks.filter((t) => t !== task);
+          this.saveTasks();
+          this._onDidChangeTreeData.fire();
+        }
+      });
+  }
 }
 
 module.exports = TaskProvider;
